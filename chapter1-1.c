e284777137cf91f70f6dc4f6500327934748263a
Mohammad-Abdullah <87905855+AbdullahWB@users.noreply.github.com>
2024-10-14 01:36:56 +0800

    chapte 1

---
+++ chapter1-1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+struct Student {
+    int student_id;
+    char name[50];
+    float mathGrades;
+    float englishGrades;
+    float computerGrades;
+    float totalGrades;
+};
+
+int main() {
+    struct Student students[3];
+    float maxNumberOfGrades = 0;
+    int topStudentIndex = 0;
+    for (int i = 0; i < 3; i++) {
+        scanf("%d%s%f%f%f", &students[i].student_id,students[i].name,&students[i].mathGrades,&students[i].englishGrades,&students[i].computerGrades);
+        students[i].totalGrades = (students[i].mathGrades + students[i].computerGrades + students[i].englishGrades) / 3;
+        if (students[i].totalGrades > maxNumberOfGrades) {
+            maxNumberOfGrades = students[i].totalGrades;
+            topStudentIndex = i; 
+        }
+    }
+    printf("%s\n", students[topStudentIndex].name);
+    return 0;
+}
